b18cdc0c582f820bee05bb331eb4731fb99d9d66
Lin <inltree@outlook.com>
2026-03-26 09:33:49 +0800

    Update script URLs in Script_Config.lua

--- Config/Script_Config.lua
+++ Config/Script_Config.lua
@@ -1,40 +1,40 @@
-local Game_Config = {
+local Config = {
     [4483381587] = {
         Name = "一个字面上的底板。｜(a literal baseplate.)",
         ScriptUrl = ""
     },
     [126884695634066] = {
         Name = "种植花园｜(Grow a Garden)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Grow_a_Garden.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Grow_a_Garden.lua"
     },
     [76455837887178] = {
         Name = "挖掘它｜(Dig it)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Dig_it.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Dig_it.lua"
     },
     [99078474560152] = {
         Name = "M.E.G.无尽现实｜(M.E.G. Endless Reality)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/M.E.G._Endless_Reality.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/M.E.G._Endless_Reality.lua"
     },
     [109983668079237] = {
         Name = "偷走一粒脑红｜(Steal a Brainrot)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Steal_a_Brainrot.lua"
     },
     [96342491571673] = {
         Name = "偷走一粒脑红｜(Steal a Brainrot)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Steal_a_Brainrot.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Steal_a_Brainrot.lua"
     },
     [5171093784] = {
         Name = "不要按按钮四｜(Don't Press The Button 4)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Don't_Press_The_Button_4.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Don't_Press_The_Button_4.lua"
     },
     [189707] = {
         Name = "自然灾害生存｜(Natural Disaster Survival)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Natural_Disaster_Survival.luau"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Natural_Disaster_Survival.luau"
     },
     [5611648039] = {
         Name = "谋杀派对｜(Murder Party)",
-        ScriptUrl = "https://raw.githubusercontent.com/inltree/INL_LIN_ROBLOX_SCRIPT/main/Game_Script/Murder_Party.lua"
+        ScriptUrl = "https://raw.githubusercontent.com/inltree/ROBLOX-SCRIPT_inl/main/Game_Script/Murder_Party.lua"
     }
 }
 
-return Game_Config
+return Config
